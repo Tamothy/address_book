93677414af7ac0efc8349638f3b4b136352fff34
Tam Nguyen <mr.tamron@gmail.com>
2016-06-27 19:36:00 -0700

    add first/last name constructor to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//business_logic
+function Contact(first, last) {
+  this.first_name = first;
+  this.last_name = last;
+}
+
+Contact.prototype.full_name = function () {
+  return this.first_name + " " + this.last_name;
+}
+
+//user_interface_logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var input_first_name = $("input#new-first-name").val();
+    var input_last_name = $("input#new-last-name").val();
+
+    var newContact = new Contact(input_first_name, input_last_name);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.full_name() + "</span></li>");
+
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.first_name);
+      $(".first-name").text(newContact.first_name);
+      $(".last-name").text(newContact.last_name);
+
+    });
+  });
+});
